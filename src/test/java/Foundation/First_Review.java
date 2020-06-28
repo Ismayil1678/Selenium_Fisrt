007078af97a9c7d364e3e803b81d9ed092f3aa9d
ismayil1678 <ismayil@na.edu>
2020-06-28 09:53:26 -0500

    Add driver.navigate() method

--- src/test/java/Foundation/First_Review.java
+++ src/test/java/Foundation/First_Review.java
@@ -19,14 +19,25 @@ public class First_Review {
         System.setProperty("webdriver.chrome.driver","C:\\DriversSelenium LB\\chromedriver.exe");
 
         //Create a Webdriver object;
-
         WebDriver driver=new ChromeDriver();
 
         //Open the website or go to the Url page
-        driver.get("https://the-internet.herokuapp.com/");
+       // driver.get("https://the-internet.herokuapp.com/");
 
-        //At the end of the test kill the browser;
-        driver.quit();
+         /*driver.get() is used  to navigate particular Url (website) and wait till page load
+
+           driver.navigate().to is used navigate particular Url and does not wait until to load page.
+           It ,maintains browser  history or cookies to navigate  back or forward*/
+           //driver.navigate().to("https://the-internet.herokuapp.com/");
+           //driver.navigate().back();
+           //driver.navigate().forward();
+
+
+
+
+       //At the end of the test kill the browser;
+         driver.quit();
+
 
 
 
